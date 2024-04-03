36639b4bf36f1912514a2bda11e0e6ff5cc0aadf
Girish Venkatachalam <girish@gayatri-hitech.com>
2024-04-03 10:46:17 +0530

    Push notebooks

---
+++ jupyter-notebook/sequence_iter.py
@@ -0,0 +1,17 @@
+# sequence_iter.py
+
+class SequenceIterator:
+    def __init__(self, sequence):
+        self._sequence = sequence
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < len(self._sequence):
+            item = self._sequence[self._index]
+            self._index += 1
+            return item
+        else:
+            raise StopIteration
